f6599dfbaad431b1b8d6534392eb44c10133f8c7
Olosunde Kehinde Ademola <kennydudez2@gmail.com>
2019-04-12 18:31:48 +0100

    Created Components, Services and Model Classes with AngularCli. Added Bootstrap and Font Awesome CSS and Javascript Links elements to the index.html.

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderComponent } from './orders/order/order.component';
+import { OrderItemsComponent } from './orders/order-items/order-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    OrderComponent,
+    OrderItemsComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/src/app/orders/order/order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styles: []
+})
+export class OrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
