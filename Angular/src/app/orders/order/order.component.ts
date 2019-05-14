6228e3562cd1ccc1ac982d847bbbf22959891b10
Olosunde Kehinde Ademola <kennydudez2@gmail.com>
2019-05-14 16:29:54 +0100

    New Commit

--- Angular/src/app/orders/order/order.component.ts
+++ Angular/src/app/orders/order/order.component.ts
@@ -1,41 +1,42 @@
+import { CustomerService } from './../../shared/customer.service';
+import { OrderService } from './../../shared/order.service';
 import { Component, OnInit } from '@angular/core';
-import { OrderService } from 'src/app/shared/order.service';
 import { NgForm } from '@angular/forms';
-import { Customer } from 'src/app/shared/customer.model';
-import { MatDialog, MatDialogConfig } from '@angular/material';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { OrderItemsComponent } from '../order-items/order-items.component';
-import { CustomerService } from 'src/app/shared/customer.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Customer } from 'src/app/shared/customer.model';
 import { ToastrService } from 'ngx-toastr';
+import { Router, ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styles: []
 })
 export class OrderComponent implements OnInit {
-  isValid: boolean = true;
   customerList: Customer[];
+  isValid: boolean = true;
+
   constructor(private service: OrderService,
-    private dialog: MatDialog, 
+    private dialog: MatDialog,
     private customerService: CustomerService,
     private toastr: ToastrService,
-    private router : Router,
-    private currentRoute: ActivatedRoute
-    ) { }
+    private router: Router,
+    private currentRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    let OrderID =  this.currentRoute.snapshot.paramMap.get('id');
-    if(OrderID==null)
-    this.resetForm();
-else {
-  this.service.getOrderByID(parseInt (OrderID)).then(res=>{
-    this.service.formData = res.order;
-    this.service.formData = res.orderDetails;
-  });
-}
+    let orderID = this.currentRoute.snapshot.paramMap.get('id');
+    if (orderID == null)
+      this.resetForm();
+    else {
+      this.service.getOrderByID(parseInt(orderID)).then(res => {
+        this.service.formData = res.order;
+        this.service.orderItems = res.orderDetails;
+      });
+    }
+
     this.customerService.getCustomerList().then(res => this.customerList = res as Customer[]);
   }
-
 
   resetForm(form?: NgForm) {
     if (form = null)
@@ -46,10 +47,11 @@ else {
       CustomerID: 0,
       PMethod: '',
       GTotal: 0,
-      DeletedOrderItemIDs: '',
+      DeletedOrderItemIDs: ''
     };
     this.service.orderItems = [];
   }
+
   AddOrEditOrderItem(orderItemIndex, OrderID) {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.autoFocus = true;
@@ -60,12 +62,15 @@ else {
       this.updateGrandTotal();
     });
   }
+
+
   onDeleteOrderItem(orderItemID: number, i: number) {
     if (orderItemID != null)
     this.service.formData.DeletedOrderItemIDs += orderItemID + ",";
-    this.service.orderItems.splice(i,1);
+    this.service.orderItems.splice(i, 1);
     this.updateGrandTotal();
   }
+
   updateGrandTotal() {
     this.service.formData.GTotal = this.service.orderItems.reduce((prev, curr) => {
       return prev + curr.Total;
@@ -73,12 +78,11 @@ else {
     this.service.formData.GTotal = parseFloat(this.service.formData.GTotal.toFixed(2));
   }
 
-
   validateForm() {
     this.isValid = true;
     if (this.service.formData.CustomerID == 0)
       this.isValid = false;
-    else if (this.service.orderItems.length==0)
+    else if (this.service.orderItems.length == 0)
       this.isValid = false;
     return this.isValid;
   }
@@ -92,5 +96,6 @@ else {
         this.router.navigate(['/orders']);
       })
     }
-}
+  }
+
 }
